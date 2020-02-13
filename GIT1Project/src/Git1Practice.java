1d5782e5daf0d073a1c118d0f09fbb99827437b0
ammadfaizi <53026680+ammadfaizi@users.noreply.github.com>
2020-02-12 20:53:57 -0500

    Update Git1Practice.java

--- GIT1Project/src/Git1Practice.java
+++ GIT1Project/src/Git1Practice.java
@@ -4,6 +4,7 @@ public class Git1Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("1st Change");
+		System.out.println("online");
 	}
 
 }
